079b5c947f16f070f3b4b98d294b0d24399da605
K444 <k4nu420@gmail.com>
2024-10-28 23:14:06 +0100

    account view update

--- users/signals.py
+++ users/signals.py
@@ -1,10 +1,33 @@
-from email.policy import default
-
 from allauth.account.signals import email_confirmed
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from allauth.account.models import EmailAddress
 from .models import AppUser
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from allauth.socialaccount.models import SocialAccount
+
+User=get_user_model()
+
+@receiver(user_signed_up)
+def google_user_profile(request,user,**kwargs):
+    """
+    Signal to populate the AppUser model or update it after a user signs up using Google
+    """
+    try:
+       social_account=SocialAccount.objects.get(user=user)
+       data=social_account.extra_data
+       firstname=data.get("given_name", "")
+       lastname=data.get('family_name',"")
+       email=data.get("email","")
+
+       user.firstname=firstname
+       user.lastname=lastname
+       user.email=email
+       user.save()
+
+    except SocialAccount.DoesNotExist:
+        pass
 
 @receiver(email_confirmed)
 def email_confirmed_callback(request,email_address, **kwargs):
