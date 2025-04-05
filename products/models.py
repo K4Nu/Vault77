383eab15f65df3f4f069fbc9ff7c097c8da225e6
K4 <k4nu420@gmail.com>
2025-04-05 19:48:34 +0200

    product model

--- products/models.py
+++ products/models.py
@@ -107,3 +107,30 @@ class Size(models.Model):
             count = Size.objects.filter(size_group=self.size_group).count()
             self.order = count
         super().save(*args, **kwargs)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    description = models.TextField(blank=True, null=True)
+    care_instructions = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        indexes = [
+            models.Index(fields=('name', 'slug'), name='product_name_idx'),
+        ]
+
+    def __str__(self):
+        return f'{self.category.name} - {self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    """
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
+    """
