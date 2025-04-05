89311214a8bd79707055425dabff9c6346ea9c89
K4 <k4nu420@gmail.com>
2025-04-05 18:40:34 +0200

    gender model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
+from mptt.models import MPTTModel, TreeForeignKey
+from mptt.managers import TreeManager
+from django.urls import reverse
 
-# Create your models here.
+class Gender(models.Model):
+    gender_name= models.CharField(max_length=100,unique=True)
+    def __str__(self):
+        return self.gender_name
---
+++ products/tests/tests.py
@@ -0,0 +1,3 @@
+import pytest
+from products.models import Gender
+
