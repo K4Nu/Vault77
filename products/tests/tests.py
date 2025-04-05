2c5326e99ab091928fd875c641851c3f05b5b099
K4 <k4nu420@gmail.com>
2025-04-05 18:56:31 +0200

    gender model tests

--- products/tests/tests.py
+++ products/tests/tests.py
@@ -1,3 +1,8 @@
 import pytest
 from products.models import Gender
 
+@pytest.mark.django_db
+class TestGenderModel:
+    def test_str_method(self):
+        gender = Gender.objects.create(gender_name='male')
+        assert str(gender) == 'male'
